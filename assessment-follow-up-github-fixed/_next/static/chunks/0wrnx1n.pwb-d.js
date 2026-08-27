7291ace1099e16113c70ed139a7dcfef4233723e
saidahkhwar11-lang <saidah.khwar11@gmail.com>
2026-08-27 17:05:17 +0400

    Add files via upload

---
+++ assessment-follow-up-github-fixed/_next/static/chunks/0wrnx1n.pwb-d.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,19581,t=>{"use strict";var e=t.i(43476),o=t.i(31713);t.s(["default",0,function(){return(0,e.jsx)(o.default,{portal:"teacher"})}])}]);
---
+++ assessment-follow-up-github-fixed/_next/static/chunks/0yuuq8sexoxdm.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,23261,t=>{"use strict";var e=t.i(43476),i=t.i(31713);t.s(["default",0,function(){return(0,e.jsx)(i.default,{portal:"admin"})}])}]);
