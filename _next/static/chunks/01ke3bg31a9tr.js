72968009e80a9f23c0b95a5457e3567063d1ef74
saidahkhwar11-lang <saidah.khwar11@gmail.com>
2026-09-09 16:41:39 +0400

    Add files via upload

---
+++ _next/static/chunks/01ke3bg31a9tr.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,57051,t=>{"use strict";var e=t.i(24903),o=t.i(93255);t.s(["default",0,function(){return(0,e.jsx)(o.default,{portal:"teacher"})}])}]);
---
+++ _next/static/chunks/182b59by29b6m.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,31896,t=>{"use strict";var e=t.i(24903),i=t.i(93255);t.s(["default",0,function(){return(0,e.jsx)(i.default,{portal:"admin"})}])}]);
